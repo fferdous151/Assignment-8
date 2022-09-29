94c405c9507ff745bc4656b020c2f55d00568f9a
fferdous151 <fferdous15@gmail.com>
2022-09-30 02:31:26 +0600

    some changes

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -13,7 +13,7 @@ const Product = (props) => {
                 <h5>Time Required: {time}s</h5>
 
             </div>
-            <button className='btn-cart'>Add To List</button>
+            <button onClick={() => props.addToCart(props.product)} className='btn-cart'>Add To List</button>
 
         </div>
     );
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,28 +4,39 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [showMenu, setShowMenu] = useState([])
     useEffect(() => {
         fetch('info.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
 
+    const addToCart = (product) => {
+        const newShow = [...showMenu, product]
+        setShowMenu(newShow);
+    }
+
     return (
         <div className='shop-container'>
             <div className='bg'>
-                <h2 className='header-bg'>FITNESS CLUB</h2>
-                <h3>Select today’s exercise</h3>
+                <div className='header-bg'>
+                    <h2>FITNESS CLUB</h2>
+                    <h3>Select today's exercise</h3>
+                </div>
                 <div className="products-container">
                     {
                         products.map(product => <Product
                             key={product.id}
                             product={product}
+                            addToCart={addToCart}
                         ></Product>)
                     }
                 </div>
             </div>
             <div className="menu-container">
-                m
+                <div className='pic-name'>
+                    <h2>Fahim Ferdous</h2>
+                </div>
             </div>
         </div>
     );
